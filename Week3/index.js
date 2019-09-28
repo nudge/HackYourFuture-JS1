4bfd714a1ff31bef6d23ed8705f1d9645e4192a3
David Ng <davidng@uber.com>
2019-09-28 20:14:23 +0200

    Add examples

---
+++ Week3/index.js
@@ -0,0 +1,37 @@
+{
+  // ----------------------------------------
+  // More examples of functions
+
+  function squared(num) {
+    return num * num;
+  }
+
+  function cubed(num) {
+    return num * num * num;
+  }
+
+  function factorial(num) {
+    var x = num;
+    while (x > 1) {
+      num *= x - 1;
+      x--;
+    }
+    return num;
+  }
+
+  // ----------------------------------------
+  // Event Handling examples
+
+  // Example of a function statement
+  function random(maxValue) {
+    return Math.floor(Math.random() * Math.floor(maxValue));
+  }
+
+  // Example of a function expression
+  var btn = document.querySelector("button");
+  btn.onclick = function() {
+    var rndCol =
+      "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+    document.body.style.backgroundColor = rndCol;
+  };
+}
